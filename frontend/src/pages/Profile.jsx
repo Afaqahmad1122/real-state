2a2cc6f000f10dc9f8d5ef4ac390028a211c5fce
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2024-06-11 04:35:56 -0700

    create profile page UI

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,48 @@
+import { useSelector } from "react-redux";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  const { currentUser } = useSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+
+      <form className="flex flex-col gap-4">
+        <img
+          src={currentUser.avatar}
+          alt="avatar image"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+          id="password"
+        />
+        <button className="bg-blue-500 text-white rounded-lg p-3 uppercase font-semibold hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+
+      <div className="flex justify-between items-center mt-5">
+        <span className="text-red-700 cursor-pointer font-bold">
+          Delete Account
+        </span>
+        <span className="text-red-700 cursor-pointer font-bold">Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
